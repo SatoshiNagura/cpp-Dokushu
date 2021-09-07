1fe74b9a96668e6ed69b03b57009a0631604caaf
SatoshiNagura <rnrnrnrg@gmail.com>
2021-09-07 19:29:51 +0900

    途中

--- sample3-3.cpp
+++ sample3-3.cpp
@@ -7,4 +7,16 @@ class Book{
   int price;
 public:
   Book(std::string title, std::string writer, int price);
+  Book(const Book& other);
+};
+
+//1の回答
+Book::Book(std::string title, std::string writer, int price){}
+
+Book::Book(const Book& other){}
+
+int main(){
+  Book novel("bocchan", "natsume", 3000);
+  Book copy(novel);
+  std::cout << copy.title << std::endl;
 }
